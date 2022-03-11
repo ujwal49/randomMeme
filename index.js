ef3dffdda2e89757876614b0134b47a87d40d71a
ujwal49 <65085816+ujwal49@users.noreply.github.com>
2022-03-11 22:52:36 +0530

    Add catch for fetch API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function newMEME() {
   let image = document.getElementById("random-image");
+  let oops = document.getElementById("oops");
+  let para = document.getElementById("oops-para");
 
   fetch("https://meme-api.herokuapp.com/gimme")
     .then((meme) => meme.json())
@@ -8,5 +10,13 @@ function newMEME() {
       console.log(meme);
 
       image.src = meme;
+    })
+    .catch((err) => {
+      image.style.display = "none";
+
+      oops.innerHTML = "OOPS :(";
+      oops.style.color = "white";
+      para.innerHTML = "Try again later";
+      para.style.color = "white";
     });
 }
